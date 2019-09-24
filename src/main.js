ca13b1c3f5b603e52c88f131b15c3aa9800803b7
Alga <akgleal@gmail.com>
2019-09-24 16:10:24 +1000

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import ElUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElUI);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
